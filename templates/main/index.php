4f0cf30d9d15141b1e7fe1bf57ead2a804b4faa1
Edil Mukambetov <47814448+Ed8L@users.noreply.github.com>
2020-11-01 20:11:31 +0600

    Update index.php

--- templates/main/index.php
+++ templates/main/index.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ .  '/../header.php'; ?>
 <?php foreach ($articles as $article): ?>
-<div class="card">
+<div class="card mt-3">
     <div class="card-body">
         <div class="title d-flex">
             <h3><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h3>
